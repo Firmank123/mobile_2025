89b46361040122786813a404c4282d883846ffd4
Firmank123 <firmankediri123@gmail.com>
2025-10-01 11:00:55 +0700

    Minggu3: praktikum1: Modifikasi sesuai langkah 3
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- Praktikum3/praktikum1.dart
+++ Praktikum3/praktikum1.dart
@@ -1,11 +1,9 @@
 void main() {
-  var list = [1, 2, 3];
-  assert(list.length == 3);
-  assert(list[1] == 2);
+  final list = [null, "M. Firmansyah", "2341720099", null, null];
+  assert(list.length == 5);
+  assert(list[1] == "M. Firmansyah");
   print(list.length);
   print(list[1]);
-
-  list[1] = 1;
-  assert(list[1] == 1);
-  print(list[1]);
+  print(list[2]);
+  print(list);
 }
