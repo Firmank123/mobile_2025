806c1d9dc329432681aa7bd4afd874f4fbf98d63
Firmank123 <firmankediri123@gmail.com>
2025-10-01 11:37:28 +0700

    Minggu3: Praktikum4: Menambahkan Null Safety pada List
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- Praktikum3/praktikum4.dart
+++ Praktikum3/praktikum4.dart
@@ -1,7 +1,13 @@
 void main() {
-  var list1 = [1, 2, 3];
+  List<int?> list1 = [1, 2, 3];
   var list2 = [0, ...list1];
   print(list1);
   print(list2);
   print(list2.length);
+
+  list1 = [1, 2, null];
+  print(list1);
+
+  var list3 = [0, ...list1];
+  print(list3.length);
 }
