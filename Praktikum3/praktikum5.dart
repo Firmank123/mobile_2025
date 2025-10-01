531de3e4bca423d78a38ed88f1f74e8776d45c2b
Firmank123 <firmankediri123@gmail.com>
2025-10-01 11:55:21 +0700

    Minggu3: praktikum5: Langkah 3
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- Praktikum3/praktikum5.dart
+++ Praktikum3/praktikum5.dart
@@ -8,6 +8,12 @@ void main() {
     var (a, b) = record;
     return (b, a);
   }
+
   var hasil = tukar((17, 29));
   print(hasil);
+
+  // Langkah 3
+  // Record type annotation in a variable declaration:
+  (String, int) mahasiswa = ('M. Firmansyah', 2341720099);
+  print(mahasiswa);
 }
