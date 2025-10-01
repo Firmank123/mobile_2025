041951cb6ddf69ee5ad0ef01fb0720856e22af4c
Firmank123 <firmankediri123@gmail.com>
2025-10-01 11:14:36 +0700

    Minggu3: praktikum2: `.add() dan .addAll()`
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- Praktikum3/praktikum2.dart
+++ Praktikum3/praktikum2.dart
@@ -1,4 +1,18 @@
-void main() {
+void main(List<String> args) {
   var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
   print(halogens);
+
+  var names1 = <String>{};
+  Set<String> names2 = {};
+
+  // .add()
+  names1.add("M. Firmansyah");
+  names1.add("2341720099");
+  names1.add("names1");
+
+  // .addAll()
+  names2.addAll({"M. Firmansyah", "2341720099", "names2"});
+
+  print(names1);
+  print(names2);
 }
