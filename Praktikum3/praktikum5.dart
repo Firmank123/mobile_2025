1616ca6688da72cee387c19971aeb7d204869a39
Firmank123 <firmankediri123@gmail.com>
2025-10-01 11:52:18 +0700

    Minggu3: praktikum5: Langkah 2 - menukar output
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- Praktikum3/praktikum5.dart
+++ Praktikum3/praktikum5.dart
@@ -1,4 +1,13 @@
 void main() {
+  // Langkah 1
   var record = ('first', a: 2, b: true, 'last');
   print(record);
+
+  // Langkah 2
+  (int, int) tukar((int, int) record) {
+    var (a, b) = record;
+    return (b, a);
+  }
+  var hasil = tukar((17, 29));
+  print(hasil);
 }
